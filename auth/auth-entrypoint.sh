4c2d9bcaee21760faf2ecccf8da7f921a8126d93
Eric Wittmann <eric.wittmann@gmail.com>
2017-11-08 13:22:41 -0500

    fix env var name issue

--- auth/auth-entrypoint.sh
+++ auth/auth-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
-    keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+if [ $OCP_KEYCLOAK_USER ] && [ $OCP_KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --user $OCP_KEYCLOAK_USER --password $OCP_KEYCLOAK_PASSWORD
 fi
 
 awk "{gsub(/OCP_UI_URL/,\"$OCP_UI_URL\");}1" /tmp/ocp-example-app-realm.json > /tmp/ocp-example-app-realm.configured.json
